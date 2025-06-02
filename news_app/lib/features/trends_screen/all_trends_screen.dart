317a2dae0726e5fe669bc5e0b4dce06abe149b86
maiaboelmagd <maishalan00000@gmail.com>
2025-06-03 00:09:08 +0300

    add trends Screen

---
+++ news_app/lib/features/trends_screen/all_trends_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:news_app/core/constants/hive_boxes_names.dart';
+import 'package:news_app/core/extensions/string_extension.dart';
+import 'package:news_app/core/provider/news_provider.dart';
+import 'package:news_app/core/widgets/news_card.dart';
+import 'package:provider/provider.dart';
+
+class AllTrendsScreen extends StatelessWidget {
+  const AllTrendsScreen({super.key});
+
+  @override
+Widget build(BuildContext context) {
+  final newsProvider = Provider.of<NewsProvider>(context);
+  return Scaffold(
+    appBar: AppBar(title: Text("Trending News")),
+    body: ValueListenableBuilder(
+      valueListenable: Hive.box(HiveBoxesNames.bookmarks).listenable(),
+      builder: (context, Box box, _) {
+        return ListView.builder(
+          itemCount: newsProvider.newsArticles.length,
+          itemBuilder: (context, index) {
+            final article = newsProvider.newsArticles[index];
+            final isBookmarked = box.containsKey(article.url);
+            return NewsCard(
+              article: article,
+              isBookmarked: isBookmarked,
+              formatTimeAgo: (time) => time.timeAgo,
+            );
+          },
+        );
+      },
+    ),
+  );
+}
+
+}
